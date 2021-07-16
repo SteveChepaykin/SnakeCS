d83aa52e49d57e8ac1020a316e4e63f1befb2603
LeFellowHuman <highgroundgamedev@gmail.com>
2021-07-16 13:47:40 +0300

    second version

--- Snake.cs
+++ Snake.cs
@@ -11,13 +11,24 @@ namespace SnakeCS
         static void Main(string[] args)
         {
             int x1 = 20;
-            int y1 = 10;
-            char sym1 = '*';
+            int y1 = 4;
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            int x2 = 4;
+            int y2 = 5;
+
+            char sym1 = '*';
+            char sym2 = '#';
+
+            Draw(x1, y1, sym1);
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
